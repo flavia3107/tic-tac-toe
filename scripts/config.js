d38f8b3bf059d20188fe349ae6b7bb34469e82a9
Flavia Kosti <flaviakosti@gmail.com>
2025-10-04 14:54:43 -0400

    TicTacToe: Redesign P2

--- scripts/config.js
+++ scripts/config.js
@@ -27,7 +27,7 @@ function savePlayerConfig(event) {
     const updatedPlayerDataElement = document.getElementById(`player-${editedPlayer}-data`);
     updatedPlayerDataElement.children[1].textContent = enteredPlayerName;
 
-    players[editedPlayer-1].name = enteredPlayerName;
-    
+    players[editedPlayer - 1].name = enteredPlayerName;
+
     closePlayerConfig();
 }
